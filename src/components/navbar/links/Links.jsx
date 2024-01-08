459424b2f9b22becdeb4bb35db98876d7c617e01
MD Hadiuzzaman <80534239+HadiAnik@users.noreply.github.com>
2024-01-08 20:13:17 +0600

    Update Links.jsx

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,5 +1,5 @@
 import styles from "../navbar.module.css";
-import NavLink from "./navLink/navLink";
+import NavLink from "./navLink/NavLink";
 
 function Links() {
   const links = [
@@ -20,11 +20,24 @@ function Links() {
       link: "/blog",
     },
   ];
+
+  // Temporary
+  const session = true;
+  const isAdmin = true;
+
   return (
     <div className={styles.nav}>
       {links.map((link, i) => (
         <NavLink item={link} key={i} />
       ))}
+      {session ? (
+        <>
+          {isAdmin && <NavLink item={{ name: "Admin", link: "/admin" }} />}
+          <span className={styles.login}>Logout</span>
+        </>
+      ) : (
+        <NavLink item={{ name: "Login", link: "/login" }} />
+      )}
     </div>
   );
 }
